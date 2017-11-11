96c4d2734d135cf137d005b880ccfa8c592cd172
KyleMondina <kylemondina@gmail.com>
2017-11-10 22:40:49 -0800

    new files

---
+++ movingBall.js
@@ -0,0 +1,11 @@
+var canvas = document.getElementById("canvas");
+
+var windowWidth = window.innerWidth;
+var windowHeight = window.innerHeight;
+
+canvas.width = windowWidth;
+canvas.height = windowHeight;
+
+const pie = 3.14;
+
+var context = canvas.getContext('2d');
